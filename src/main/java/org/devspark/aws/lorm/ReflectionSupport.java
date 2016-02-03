9a5dd7610e78aa19d18b619d078e8a429cbb15d3
Javier Storni <storni@gmail.com>
2016-02-03 11:13:19 -0300

    Moved to devspark repositories and minor update in enumerations support

--- src/main/java/org/devspark/aws/lorm/ReflectionSupport.java
+++ src/main/java/org/devspark/aws/lorm/ReflectionSupport.java
@@ -12,100 +12,104 @@ import org.devspark.aws.lorm.exceptions.DataException;
 
 public class ReflectionSupport {
 
-    public Field getFieldWithAnnotation(Class<?> sourceClass,
-	    Class<? extends Annotation> annotationClass) {
-	Field field = null;
-	List<Field> fields = getAllFields(sourceClass);
-	for (Field currentField : fields) {
-	    Annotation annotation = currentField.getAnnotation(annotationClass);
-	    if (annotation != null) {
-		field = currentField;
-		break;
-	    }
-	}
-
-	if (field == null) {
-	    throw new DataException("Id attribute not found in class " + sourceClass.getName());
-	}
-
-	return field;
-    }
-
-    public Object getValueOfField(Field field, Object instance) {
-	Object value;
-	try {
-	    if (!field.isAccessible()) {
-		field.setAccessible(true);
-	    }
-
-	    value = field.get(instance);
-	} catch (Exception ex) {
-	    throw new DataException("Error while obtaining value from field [" + field.getName()
-		    + "] and class [" + instance.getClass().getName() + "]", ex);
-	}
-
-	return value;
-    }
-
-    public void setValueOfField(Field field, Object instance, Object value) {
-	try {
-	    if (!field.isAccessible()) {
-		field.setAccessible(true);
-	    }
-
-	    if (field.getType().isEnum()) {
-		Object[] enumConstants = field.getType().getEnumConstants();
-		Object enumValue = null;
-		for (Object enumConstant : enumConstants) {
-		    if (enumConstant.toString().equals(value)) {
-			enumValue = enumConstant;
-			break;
-		    }
+	public Field getFieldWithAnnotation(Class<?> sourceClass,
+			Class<? extends Annotation> annotationClass) {
+		Field field = null;
+		List<Field> fields = getAllFields(sourceClass);
+		for (Field currentField : fields) {
+			Annotation annotation = currentField.getAnnotation(annotationClass);
+			if (annotation != null) {
+				field = currentField;
+				break;
+			}
 		}
-		field.set(instance, enumValue);
-	    } else {
-		field.set(instance, value);
-	    }
 
-	} catch (Exception ex) {
-	    throw new DataException("Error while assigning value [" + value + "] to field ["
-		    + field.getName() + "] and class [" + instance.getClass().getName() + "]", ex);
-	}
-    }
+		if (field == null) {
+			throw new DataException("Id attribute not found in class "
+					+ sourceClass.getName());
+		}
 
-    public List<Field> getAllFields(Class<?> sourceClass) {
-	List<Field> fields = new ArrayList<Field>();
-
-	Class<?> currentClass = sourceClass;
-	while (currentClass != null && !currentClass.equals(Object.class)) {
-	    fields.addAll(Arrays.asList(currentClass.getDeclaredFields()));
-	    currentClass = currentClass.getSuperclass();
+		return field;
 	}
 
-	return fields;
-    }
+	public Object getValueOfField(Field field, Object instance) {
+		Object value;
+		try {
+			if (!field.isAccessible()) {
+				field.setAccessible(true);
+			}
 
-    public Set<String> getAllFieldNames(Class<?> sourceClass) {
-	Set<String> fieldNames = new HashSet<String>();
+			value = field.get(instance);
+		} catch (Exception ex) {
+			throw new DataException("Error while obtaining value from field ["
+					+ field.getName() + "] and class ["
+					+ instance.getClass().getName() + "]", ex);
+		}
 
-	List<Field> fields = getAllFields(sourceClass);
-	for (Field field : fields) {
-	    fieldNames.add(field.getName());
+		return value;
 	}
 
-	return fieldNames;
-    }
+	public void setValueOfField(Field field, Object instance, Object value) {
+		try {
+			if (!field.isAccessible()) {
+				field.setAccessible(true);
+			}
 
-    public <T> T instantiate(Class<T> clazz) {
-	T instance;
+			if (field.getType().isEnum()) {
+				Object[] enumConstants = field.getType().getEnumConstants();
+				Object enumValue = null;
+				for (Object enumConstant : enumConstants) {
+					if (enumConstant.toString().equals(value)) {
+						enumValue = enumConstant;
+						break;
+					}
+				}
+				field.set(instance, enumValue);
+			} else {
+				field.set(instance, value);
+			}
 
-	try {
-	    instance = clazz.newInstance();
-	} catch (Exception ex) {
-	    throw new DataException("Cannot instantiate: " + clazz.getName(), ex);
+		} catch (Exception ex) {
+			throw new DataException("Error while assigning value [" + value
+					+ "] to field [" + field.getName() + "] and class ["
+					+ instance.getClass().getName() + "]", ex);
+		}
 	}
 
-	return instance;
-    }
+	public List<Field> getAllFields(Class<?> sourceClass) {
+		List<Field> fields = new ArrayList<Field>();
+
+		Class<?> currentClass = sourceClass;
+		while (currentClass != null && !currentClass.equals(Object.class)) {
+			fields.addAll(Arrays.asList(currentClass.getDeclaredFields()));
+			currentClass = currentClass.getSuperclass();
+		}
+
+		return fields;
+	}
+
+	public Set<String> getAllFieldNames(Class<?> sourceClass) {
+		Set<String> fieldNames = new HashSet<String>();
+
+		List<Field> fields = getAllFields(sourceClass);
+		for (Field field : fields) {
+			fieldNames.add(field.getName());
+		}
+
+		return fieldNames;
+	}
+
+	public <T> T instantiate(Class<T> clazz) {
+		T instance;
+
+		try {
+			instance = clazz.newInstance();
+		} catch (Exception ex) {
+			throw new DataException("Cannot instantiate: " + clazz.getName(),
+					ex);
+		}
+
+		return instance;
+	}
 
 }
